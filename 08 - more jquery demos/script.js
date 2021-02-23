3efbffd4da2b3515234ad2f056ba78f27e776ace
alisq <ali.shamas.qadeer@gmail.com>
2021-02-23 18:26:50 -0500

    fff

--- 08 - more jquery demos/script.js
+++ 08 - more jquery demos/script.js
@@ -12,7 +12,24 @@ $(document).scroll(function(){
 
 	fromTop = $(document).scrollTop();
 
-	scrollRatio = fromTop/(docHeight-winHeight)
+	scrollRatio = fromTop/(docHeight-winHeight);
+
+	//CHECK THIS CODE OUT SHERYSSE!
+	if ((fromTop+(winHeight/2)) >  $(".hidden").offset().top ) {
+		$(".hidden").css({"opacity":1})
+	} else {
+		$(".hidden").css({"opacity":0})
+	}
+
+
+	//LOOK AT THIS TO DO IT FOR A BUNCH OF DIFFERENT ELEMENTS!
+	 $("p").each(function(){
+		if ((fromTop+(winHeight/2)) >  $(this).offset().top ) {
+			$(this).css({"opacity":1})
+		} else {
+			$(this).css({"opacity":0})
+		}
+	 })
 
 	
 	$(".pseudo-pic")
@@ -23,13 +40,7 @@ $(document).scroll(function(){
 
 
 
-			if ((fromTop+(winHeight/2)) >  $(".hidden").offset().top ) {
-				$(".hidden").css({"opacity":1})
-			} else {
-				$(".hidden").css({"opacity":0})
-			}
-
-
+			
 
 
 
